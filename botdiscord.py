b6bcc4d870047c6d03b1e25b7bcc3176b62e72c4
Rafael Carvalho <99702583+RafaCarvalh0@users.noreply.github.com>
2022-02-15 00:16:49 -0300

    Create botdiscord.py

---
+++ botdiscord.py
@@ -0,0 +1,28 @@
+import discord
+import os
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+        if message.content == '?regras':
+            await message.channel.send(f'{message.author.name} as regras do servidor são:{os.linesep}1- teste{os.linesep}2- teste')
+        elif message.content == '?nivel':
+            await message.author.send('Nível 1')
+
+    async def on_member_join(self, member):
+        guild = member.guild
+        if guild.system_channel is not None:
+            mensagem = f'{member.mention} acabou de entrar no {guild.name}'
+            await guild.system_channel.send(mensagem)
+
+
+intents = discord.Intents.default()
+intents.members = True
+
+
+client = MyClient(intents=intents)
+client.run('_codigo do servidor discord_')
